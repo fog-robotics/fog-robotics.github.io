6c76af0f7474ae510e6914163137ec8d09061592
yousefh409 <yousefh409@gmail.com>
2023-09-28 23:42:15 -0700

    cool animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,21 +22,23 @@ const Home = () => {
                 </div>
             </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <ul class="tree">
-                <li>
-                    <li> <span><TreeNode name="FogROS2-Sky" path="/fogros-sky" description="Optmizing time and Cost for Cloud Robotics" /></span></li>
-                    <li> <span><TreeNode name="FogROS-SGC" path="/fogros-sgc" description="FogROS2 for secure global connectivity" /></span></li>
-                    <li> <span><TreeNode name="FogROS-LS" path="/fogros-ls" description=" Location-Independent framework for  Latency Sensitive Robotics" /></span></li>
-                    <ul>
-                        <li> 
-                            <span><TreeNode name="FogROS2" path="/fogros2" description="An upgraded version of FogROS2 for ROS2" /></span>
-                            <ul>
-                                <li><span><TreeNode name="FogROS" path="/fogros" description="The original  FogROS" /></span></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <div className={homeStyles.treeWrapper}>
+                <ul class="tree">
+                    <li>
+                        <li> <span><TreeNode name="FogROS2-Sky" path="/fogros-sky" description="Optmizing time and Cost for Cloud Robotics" /></span></li>
+                        <li> <span><TreeNode name="FogROS-SGC" path="/fogros-sgc" description="FogROS2 for secure global connectivity" /></span></li>
+                        <li> <span><TreeNode name="FogROS-LS" path="/fogros-ls" description=" Location-Independent framework for  Latency Sensitive Robotics" /></span></li>
+                        <ul>
+                            <li> 
+                                <span><TreeNode name="FogROS2" path="/fogros2" description="An upgraded version of FogROS2 for ROS2" /></span>
+                                <ul>
+                                    <li><span><TreeNode name="FogROS" path="/fogros" description="The original  FogROS" /></span></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
             <div className={homeStyles.cloudRoboticsWrapper}>
                 <h1>What is cloud robotics?</h1>
                 <p>Cloud robotics encompasses many relations between the cloud and robotics. Here we focus on what the cloud offers, explicitly focusing on the capabilities FogROS 2 can give robots by using the cloud.</p>
